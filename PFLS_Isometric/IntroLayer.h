2656ee8e2bf3e32742e50b4181f82ab8683c9d4e
ruben flores <rubenator.flores@gmail.com>
2013-05-22 14:21:06 -0500

    Initial Commit

---
+++ PFLS_Isometric/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  PFLS_Isometric
+//
+//  Created by Ruben Flores on 5/22/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ PFLS_Isometric/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PFLS_Isometric
+//
+//  Created by Ruben Flores on 5/22/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
