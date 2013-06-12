69afa8e2b5ccc373d7c51ad4a3314092f8af1579
ruben flores <rubenator.flores@gmail.com>
2013-06-12 14:52:56 -0500

    Collision and Edge detection
    added collision and edge dectection.
    added very simple straight path finiding where character will move
    along one axis until it hits a barrier.
    added a new type TileDirection to indicate which direction to move

---
+++ PFLS_Isometric/GameLevelDelegate.h
@@ -0,0 +1,13 @@
+//
+//  GameLevelDelegate.h
+//  PFLS_Isometric
+//
+//  Created by Ruben Flores on 6/12/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol GameLevelDelegate <NSObject>
+
+@end
---
+++ PFLS_Isometric/GameTypes.h
@@ -0,0 +1,14 @@
+//
+//  GameTypes.h
+//  PFLS_Isometric
+//
+//  Created by Ruben Flores on 6/12/13.
+//
+//
+
+#ifndef PFLS_Isometric_GameTypes_h
+#define PFLS_Isometric_GameTypes_h
+
+
+
+#endif
