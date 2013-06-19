8e4aef87d8343789a580cb1650cf77e86b2ca0a5
Thomas Hocking <thomashocking7@me.com>
2013-06-19 13:50:38 -0500

    Commit

--- PFLS_Isometric/GameConstants.h
+++ PFLS_Isometric/GameConstants.h
@@ -12,9 +12,13 @@
 /* Game Time Constants */
 #define kTILE_MOVEMENT_SPEED 0.25f //Velocity of player (sec/tile)
 
-
 /* Game TMX file names  should be kLEVELNAME_MAP_NAME*/
-#define kTEST_LEVEL_MAP_NAME @"thomasmap.tmx"
+
+#define kTEST_LEVEL_MAP_NAME @"Maze.tmx"
+
+/* Tiled Map Layer names */
+#define kGROUND_LAYER   @"Floor"
+#define kBARRIERS_LAYER  @"Barriers"
 
 /* Game Touch dispatcher priorities */
 #define kPLAYER_PRIORITY 0
